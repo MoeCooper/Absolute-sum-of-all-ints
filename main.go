751b2cef7a8e4163b47d37b8b4531e88d15a72fb
Maurice cooper <jc70240@gmail.com>
2020-11-04 07:27:01 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func sumNums(arr[] int) int64 {
+    var sum int64 = 0
+    for i := 0; i < len(arr); i++ {
+        sum += int64(math.Abs(float64(arr[i])))
+    }
+    return sum
+}
+
+func main() {
+    myArr := []int{1,2,-3,4,5}
+    fmt.Println(sumNums(myArr))
+}
